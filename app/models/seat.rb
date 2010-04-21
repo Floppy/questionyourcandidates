b9f2814c6ccb2d4a09d420e44d028b4afcee7794
James Smith <james@floppy.org.uk>
2010-04-21 22:47:09 +0100

    order events by date and candidates by name

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,8 +1,8 @@
 class Seat < ActiveRecord::Base
   validates_uniqueness_of :name, :code
 
-  has_many :candidates
-  has_many :events
+  has_many :candidates, :order => :name
+  has_many :events, :order => :starting_at
 
   def to_param
     code
